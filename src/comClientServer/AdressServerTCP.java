2a6566c03d9f569ab222f3a34f0ef7a50cd4bde2
Stephane Ielsch <stephane.ielsch@univ-fcomte.fr>
2017-12-04 10:36:33 +0100

    getter setter adressServerTCP

--- src/comClientServer/AdressServerTCP.java
+++ src/comClientServer/AdressServerTCP.java
@@ -2,4 +2,29 @@ package comClientServer;
 
 public class AdressServerTCP {
 
+	private int port;
+	private String host;
+
+	public AdressServerTCP(int port, String host) {
+		super();
+		this.port = port;
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
 }
