9080aae896eb5cf21848ffb308ec9a43ab3a2d35
Enzo-M <enzomena@gmail.com>
2017-12-05 11:29:02 +0100

    CompareTo, directoryToListFile

--- src/comClientServer/AdressServerTCP.java
+++ src/comClientServer/AdressServerTCP.java
@@ -1,30 +1,44 @@
 package comClientServer;
 
-public class AdressServerTCP {
+public class AdressServerTCP implements Comparable<AdressServerTCP> {
 
-	private int port;
-	private String host;
+    private int port;
+    private String host;
 
-	public AdressServerTCP(int port, String host) {
-		super();
-		this.port = port;
-		this.host = host;
-	}
+    public AdressServerTCP(int port, String host) {
+        super();
+        this.port = port;
+        this.host = host;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public String getHost() {
-		return host;
-	}
+    public String getHost() {
+        return host;
+    }
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int compareTo(AdressServerTCP adr) {
+        int compare = this.getHost().compareTo(adr.getHost());
+        if (compare == 0) {
+            if (this.getPort() < adr.getPort()) {
+                compare = -1;
+            } else if (this.getPort() > adr.getPort()) {
+                compare = 1;
+            } else {
+                compare = 0;
+            }
+        }
+        return compare;
+    }
 
 }
