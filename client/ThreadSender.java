c515e9bfd36045a6d0dc80adf2805e2f6ffeffeb
Stephane Ielsch <stephane.ielsch@univ-fcomte.fr>
2018-01-10 13:23:55 +0100

    thread sender avancé, il reste à gérer le dernier paquet

--- client/ThreadSender.java
+++ client/ThreadSender.java
@@ -7,63 +7,70 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
 
-import com.sun.istack.internal.ByteArrayDataSource;
+
 
 import comClientServer.AdressServerTCP;
 import comClientServer.P2PFile;
+import comClientServer.P2PParametre;
 
-public class ThreadSender extends Thread{
+public class ThreadSender extends Thread {
 
-	
-	
-	private DatagramSocket socketUDP=null;
-	private P2PFile fileToSend=null; 
-	private AdressServerTCP adress=null;
+	private DatagramSocket socketUDP = null;
+	private P2PFile fileToSend = null;
+	private String host = null;
+	private int port;
 	private DatagramPacket packetSended;
 	private byte[] buff;
 
 	private int buffSize;
 
-	
-
 	private long premierMorceau;
 	private long dernierMorceau;
-	private byte [] longInByte;
+	private byte[] longInByte;
 
-	
 	private FileInputStream fis;
 	private BufferedInputStream bis;
-	
-	public ThreadSender(P2PFile fileToSend, AdressServerTCP adress, int port,long premierMorceau, long dernierMorceau){
-		this.fileToSend=fileToSend;
-		this.adress=adress;
-		this.premierMorceau=premierMorceau;
-		this.dernierMorceau=dernierMorceau;
-		buff= new byte[buffSize];
+
+	public ThreadSender(P2PFile fileToSend, String host, int port, long premierMorceau,
+			long dernierMorceau) {
+		this.fileToSend = fileToSend;
+		this.host = host;
+		this.port=port;
+		this.premierMorceau = premierMorceau;
+		this.dernierMorceau = dernierMorceau;
+		buff = new byte[buffSize];
 
 	}
-	
-	public void run(){
-		try {
-			fis=new FileInputStream(fileToSend.getName());
-			bis=new BufferedInputStream(fis);
-			
-		
-			for (long i = premierMorceau; i < dernierMorceau; i++) {
 
-				for (int j = buffSize-Long.BYTES; j < buff.length; j++) {
+	public void run() {
+		try {
+			fis = new FileInputStream(fileToSend.getName());
+			bis = new BufferedInputStream(fis);
+
+			for (long i = premierMorceau; i < dernierMorceau; i++) {
+				
+				while (bis.read(buff, 0, P2PParametre.TAILLE_PAQUET) > 0) {
+					//ajoute la place du packet dans le fichier
+					longInByte = longToBytes(i);
+					int k=0;
+					for (int j = buffSize - Long.BYTES; j < buff.length; j++) {
+						buff[j]=longInByte[k];
+						k++;
+					}
 					
+					socketUDP.send(new DatagramPacket(buff, buff.length, InetAddress.getByName(host), port));
 				}
 				
 				
+
 			}
 
-			
-			socketUDP=new DatagramSocket();
-//			socketUDP.send(new DatagramPacket(buf, length));
+			socketUDP = new DatagramSocket();
+			// socketUDP.send(new DatagramPacket(buf, length));
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -75,11 +82,11 @@ public class ThreadSender extends Thread{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public byte[] longToBytes(long x) {
-	    ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-	    buffer.putLong(x);
-	    return buffer.array();
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.putLong(x);
+		return buffer.array();
 	}
-	
+
 }
